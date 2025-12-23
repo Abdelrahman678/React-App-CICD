1b63701bb7b27674dbd2a91956c047c4cc7d7fd0
Mohamed Osama <141430040+mohammedd2510@users.noreply.github.com>
2025-12-23 12:57:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hello from "./components/Hello";
-const AWS_KEY = "AKIAIOSFODNN7EXAMPLE";
+const AWS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
 export default function App() {
   return (<div className="app"><h1>React Static Site</h1><Hello name="railway" /></div>);
 }
